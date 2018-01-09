1c5916a1087d6d279b587873ec0dc7551cf2cec4
Administrador <umgp150992@gmail.com>
2018-01-09 00:00:09 -0600

    basic

--- methods/legislatorfind.js
+++ methods/legislatorfind.js
@@ -77,7 +77,7 @@ module.exports = module.export =
 			//  var myobj = JSON.parse(res);
 			//  console.log(myobj);
 
-  var q = app.models[ "legislators" ].find(req.body).sort( { "legislator_score_sil": -1 }  );//);
+  var q = app.models[ "legislators_basic" ].find(req.body).sort( { "legislator_score_sil": -1 }  );//);
   if ( ! _.isEmpty( req.body.populate ) )
   {
     _.each( req.body.populate, function (p) {
